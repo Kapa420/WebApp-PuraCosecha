dd787a0423e2640e0d73c592f747682d88dc88a1
ankar <caarevalo@unal.edu.co>
2021-11-26 03:03:05 +0300

    Separacion para el BackEnd y para el Frontend

--- src/config/config.ts
+++
@@ -1,9 +0,0 @@
-export default
-{
-    PORT: process.env.PORT || 3000,
-    DB_HOST: process.env.DB_HOST || 'localhost',
-    DB_USER: process.env.DB_USER || 'root',
-    DB_PASSWORD: process.env.DB_PASSWORD || 'tatto2002',
-    DATABASE: process.env.DATABASE || 'appweb1',
-    DB_PORT: process.env.DB_PORT || 3306
-}
