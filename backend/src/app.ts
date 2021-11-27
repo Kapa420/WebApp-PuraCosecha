4df73a78d57df7d10f3dfc479a0e3102686788f4
ankar <caarevalo@unal.edu.co>
2021-11-28 02:09:27 +0300

    cambios en el app.ts del BackEnd

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,12 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use((_, res, next)=> {
-  res.header('Access-Control-Allow-Origin','*');
-  res.header('Access-Control-Allow-Headers','*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
+app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
 });
 
 clienteRoutes(app)
